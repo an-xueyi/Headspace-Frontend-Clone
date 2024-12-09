04e7b7bd53c4a802fc9d1eda1a657c6bdeabf431
an-xueyi <xan@terpmail.umd.edu>
2024-12-08 21:14:22 -0500

    Add button on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import DropdownMenu from './components/DropdownMenu'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
+import DropdownMenu from "./components/DropdownMenu";
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 const Navbar = () => {
   return (
-    <header className="bg-white w-full">
+    <header className="bg-white w-ful">
       <nav
         aria-label="Global"
-        className="mx-auto flex justify-between items-center p-6 h-20"
+        className="mx-auto flex justify-between items-center px-40 h-20"
       >
-        <div className="flex">
+        <div className="flex px-10">
           <a href="#">
             <img
               src="https://images.ctfassets.net/v3n26e09qg2r/7CSyedyArzTd5GP9LiuL78/f5a303f777ee3691a8ddfd5d36a7d591/headspace-logo.svg"
@@ -15,7 +15,7 @@ const Navbar = () => {
           </a>
         </div>
 
-        <div className="flex items-center">
+        <div className="flex items-center px-6">
           <ul className="flex h-20 space-x-8">
             <li className="flex items-center">
               <a href="https://www.headspace.com/articles?origin=navigation">
@@ -45,7 +45,7 @@ const Navbar = () => {
           </ul>
         </div>
 
-        <div className="flex items-center font-light p-6">
+        <div className="flex items-center font-light px-6">
           <ul className="flex h-20 space-x-6">
             <li className="flex items-center">
               <a href="https://www.headspace.com/subscriptions?origin=navigation">
@@ -73,6 +73,14 @@ const Navbar = () => {
               </a>
             </li>
           </ul>
+        </div>
+
+        <div className="flex items-center px-6">
+          <a href="https://checkout.headspace.com/checkout?voucherCode=B2C14DANNUAL2022&origin=navigation">
+            <button type="button" className="bg-blue-600 hover:bg-blue-800 text-white font-bold rounded-full px-6 py-3">
+              Try for free
+            </button>
+          </a>
         </div>
       </nav>
     </header>
